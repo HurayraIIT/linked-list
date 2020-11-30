b18a715c43cee43347c120c93aed6a1794031650
Abu Hurayra <hurayraiit@gmail.com>
2020-11-30 15:37:47 +0600

    Created linked list outline, added goals(TODO) in readme

---
+++ LinkedList.h
@@ -0,0 +1,36 @@
+#pragma once
+
+class Node
+{
+    public:
+
+    int data;
+    Node *next;
+
+    Node (int val)
+    {
+        data = val;
+        next = 0;
+    }
+};
+
+class LinkedList
+{
+    public:
+
+    Node *head = 0;
+    LinkedList() { head = 0; }
+
+    // TODO: Print linked list
+    // TODO: Insertion at the head
+    // TODO: Insertion at the end
+    // TODO: Insertion at given index
+    // TODO: Delete from the head
+    // TODO: Delete from the end
+    // TODO: Delete given value
+    // TODO: Delete given index
+    // TODO: Find middle element using 1 traversal
+    // TODO: Find the maximum element
+    // TODO: Find the minimum element
+
+};
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # linked-list
 Implementation of cool linked list features I am learning.
+
+# TODO:
+
+1. Implement Singly Linked List
+2. Implement Circular Linked List
+3. Implement Queue using 2 stacks
+4. Implement Stack using 2 queues
