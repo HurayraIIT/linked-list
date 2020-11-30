03b305635cd5dfa17931f05fb6a56a21c21c4d57
Abu Hurayra <hurayraiit@gmail.com>
2020-11-30 16:00:12 +0600

    Implemented function for printing linked list

--- LinkedList.h
+++ LinkedList.h
@@ -19,9 +19,7 @@ class LinkedList
     public:
 
     Node *head = 0;
-    LinkedList() { head = 0; }
 
-    // TODO: Print linked list
     // TODO: Insertion at the head
     // TODO: Insertion at the end
     // TODO: Insertion at given index
@@ -32,5 +30,30 @@ class LinkedList
     // TODO: Find middle element using 1 traversal
     // TODO: Find the maximum element
     // TODO: Find the minimum element
+
+
+    void PrintList (void)
+    {
+        Node *currentNode = head; // Set head as current node
+
+        // Check if current node is empty
+        if (currentNode==0)
+        {
+            std::cout << "Linked List is Empty.\n";
+            return;
+        }
+
+        // Start printing the linked list
+        std::cout << "List: ";
+
+        // Keep printing each node untill current node is the last node 
+        while (currentNode->next!=0)
+        {
+            std::cout << currentNode->data << " -> ";
+            currentNode = currentNode->next;
+        }
+        // Print the last node
+        std::cout << currentNode->data << " -> \n";
+    }
 
 };
