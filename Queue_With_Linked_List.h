f493f4c4f9aa6bde02c87e1cf6be4cafb6a689a0
Abu Hurayra <hurayraiit@gmail.com>
2020-12-02 08:53:51 +0600

    Create Queue_With_Linked_List.h

---
+++ Queue_With_Linked_List.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "Linked_List.h"
+
+
+class QueueWithLinkedList
+{
+    public:
+
+    QueueWithLinkedList (void) {}
+
+    LinkedList queue;
+
+    void PrintQueue (void)
+    {
+        queue.PrintList();
+    }
+
+    void Enqueue (int val)
+    {
+        queue.InsertAtTheEnd(val);
+    }
+
+    void Dequeue (void)
+    {
+        queue.deleteHead();
+    }
+
+    bool isEmpty (void)
+    {
+        return queue.isEmpty();
+    }
+};
