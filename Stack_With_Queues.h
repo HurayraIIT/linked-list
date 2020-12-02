a6fc1764ceccbb2dd4e3f1c5318e950308f2a86c
Abu Hurayra <hurayraiit@gmail.com>
2020-12-02 09:27:57 +0600

    fixed front element

--- Stack_With_Queues.h
+++ Stack_With_Queues.h
@@ -6,5 +6,43 @@ class StackWithQueues
 {
     public:
 
+    QueueWithLinkedList input, output;
+
     StackWithQueues(void) {}
+
+    void Push (int val)
+    {
+        input.Enqueue(val);
+    }
+
+    void Pop (void)
+    {
+        // Transfer all data from input to output
+        while(!input.isEmpty())
+        {
+            output.Enqueue(input.frontElement());
+            input.Dequeue();
+        }
+
+        // dequeue output
+        output.Dequeue();
+
+        // return all data from output to input
+        while (!output.isEmpty())
+        {
+            input.Enqueue(output.frontElement());
+            output.Dequeue();
+        }
+    }
+
+    bool isEmpty (void)
+    {
+        return input.isEmpty();
+    }
+
+    void PrintStack (void)
+    {
+        input.PrintQueue();
+    }
+
 };
